92cad236e2af906520da90f8fb76db68ee63158f
rook37 <33361387+rook37@users.noreply.github.com>
2025-04-06 21:43:22 -0500

    Initial react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './Home'
+import Roster from './Roster'
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-6 p-6">
+      <h1 className="text-4xl font-bold text-blue-600">Team Pokemon's Training Board</h1>
+    </div>
+  )
+}
+
+export default App
