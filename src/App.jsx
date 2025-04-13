fc2d259ce7b262fe13a74755ccd5bf5d232820fd
rook37 <33361387+rook37@users.noreply.github.com>
2025-04-12 21:23:42 -0500

    Adding background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Roster from './Roster'
 import logo from './assets/ash and pikachus training board.png'
+import bg from './assets/pokemonbgredfade.png'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-6 p-6">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-6 p-6" 
+    style={{
+      backgroundImage: `url(${bg})`,
+      backgroundRepeat: 'repeat',
+      backgroundSize: '30rem 30rem', 
+    }}
+    
+>
       <img src={logo}/>
       <Roster/>
     </div>
